b363f15f2432b0c150742f802d2740e0ca14e34f
kumaarpranv <kumaarpranv@gmail.com>
2020-02-12 14:14:09 +0530

    yes

--- lib/start_vid.dart
+++ lib/start_vid.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
-  import 'dart:io';
-  import 'package:path_provider/path_provider.dart';
-  import 'package:firebase_ml_vision/firebase_ml_vision.dart';
-  import 'package:camera/camera.dart';
-  import 'package:flutter/material.dart';
-  import 'package:path/path.dart' show join;
-  //import 'package:video_player/video_player.dart';
-  import 'package:fluttertoast/fluttertoast.dart';
-  import 'package:flutter/services.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'dart:developer';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'package:image/image.dart' as imglib;
 
 
   class TakeVid extends StatefulWidget {
@@ -20,13 +21,15 @@ import 'dart:async';
   List<Rect> _faces;
   class _TakeVidState extends State<TakeVid> {
     CameraController controller;
-    String videoPath;
+
   
     List<CameraDescription> cameras;
     int selectedCameraIdx;
     
     final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  
+    
+
+
     @override
     void initState() {
       super.initState();
@@ -34,6 +37,12 @@ import 'dart:async';
       //_faces.add(Rect.fromLTRB(50.0,100.0,140.0,200.0));
       // Get the listonNewCameraSelected of available cameras.
       // Then set the first camera as selected.
+      if(controller != null)
+      {
+        controller.stopImageStream();
+        controller.dispose();
+      }
+
       availableCameras()
           .then((availableCameras) {
         cameras = availableCameras;
@@ -43,9 +52,11 @@ import 'dart:async';
             selectedCameraIdx = 1;
           });
   
-          _onCameraSwitched(cameras[selectedCameraIdx]).then((void v) {});
+          _onCameraSwitched(cameras[selectedCameraIdx]);
         }
       })
+
+      
           .catchError((err) {
         print('Error: $err.code\nError Message: $err.message');
       });
@@ -96,34 +107,121 @@ import 'dart:async';
       );
     }
 
-    void findBound() async
-    {
-      final path = join(
-              // Store the picture in the temp directory.
-              // Find the temp directory using the `path_provider` plugin.
-              (await getTemporaryDirectory()).path,
-              'temp.png',
-            );
 
-    if(await File(path).exists()) {
-      File(path).delete();
+/*
+Future<Image> convertImg(CameraImage image) async {
+  try {
+    final int width = image.width;
+    final int height = image.height;
+    final int uvRowStride = image.planes[1].bytesPerRow;
+    final int uvPixelStride = image.planes[1].bytesPerPixel;
+
+    print("uvRowStride: " + uvRowStride.toString());
+    print("uvPixelStride: " + uvPixelStride.toString());
+
+    // imgLib -> Image package from https://pub.dartlang.org/packages/image
+    var img = imglib.Image(width, height); // Create Image buffer
+
+    // Fill image buffer with plane[0] from YUV420_888
+    for(int x=0; x < width; x++) {
+      for(int y=0; y < height; y++) {
+        final int uvIndex = uvPixelStride * (x/2).floor() + uvRowStride*(y/2).floor();
+        final int index = y * width + x;
+
+        final yp = image.planes[0].bytes[index];
+        final up = image.planes[1].bytes[uvIndex];
+        final vp = image.planes[2].bytes[uvIndex];
+        // Calculate pixel color
+        int r = (yp + vp * 1436 / 1024 - 179).round().clamp(0, 255);
+        int g = (yp - up * 46549 / 131072 + 44 -vp * 93604 / 131072 + 91).round().clamp(0, 255);
+        int b = (yp + up * 1814 / 1024 - 227).round().clamp(0, 255);     
+        // color: 0x FF  FF  FF  FF 
+        //           A   B   G   R
+        img.data[index] = (0xFF << 24) | (b << 16) | (g << 8) | r;
+      }
     }
-    imageCache.clear();
+
+    imglib.PngEncoder pngEncoder = new imglib.PngEncoder(level: 0, filter: 0);
+    List<int> png = pngEncoder.encodeImage(img);
+    
+    return Image.memory(png);  
+  } catch (e) {
+    print(">>>>>>>>>>>> ERROR:" + e.toString());
+  }
+  return null;
+}
+
+
+Future<List<int>> convertImgBytes(CameraImage image) async {
+  try {
+    imglib.Image img;
+  img = imglib.Image(image.width, image.height); // Create Image buffer
+
+  Plane plane = image.planes[0];
+  const int shift = (0xFF << 24);
+
+  // Fill image buffer with plane[0] from YUV420_888
+  for (int x = 0; x < image.width; x++) {
+    for (int planeOffset = 0;
+        planeOffset < image.height * image.width;
+        planeOffset += image.width) {
+      final pixelColor = plane.bytes[planeOffset + x];
+      // color: 0x FF  FF  FF  FF
+      //           A   B   G   R
+      // Calculate pixel color
+      var newVal = shift | (pixelColor << 16) | (pixelColor << 8) | pixelColor;
+
+      img.data[planeOffset + x] = newVal;
+    }
+  }
+
+    imglib.PngEncoder pngEncoder = new imglib.PngEncoder();
+
+    // Convert to png
+    List<int> png = pngEncoder.encodeImage(img);
+    return png;
+  } catch (e) {
+    print(">>>>>>>>>>>> ERROR:" + e.toString());
+  }
+  return null;
+}
+*/
+
+
+
+    void findBound(CameraImage availableImage) async
+    {
+      
 
     //await new Future.delayed(const Duration(seconds : 1));
     // Attempt to take a picture and log where it's been saved.
-    await controller.takePicture(path);
-    
+    //await controller.takePicture(path);
+   
+    final FirebaseVisionImageMetadata metadata = FirebaseVisionImageMetadata(
+      rawFormat: availableImage.format.raw,
+      size: Size(availableImage.width.toDouble(),availableImage.height.toDouble()),
+      planeData: availableImage.planes.map((currentPlane) => FirebaseVisionImagePlaneMetadata(
+        bytesPerRow: currentPlane.bytesPerRow,
+        height: currentPlane.height,
+        width: currentPlane.width
+        )).toList(),
+      //rotation: ImageRotation.rotation90
+      );
+  
+    //List<int> bt = await convertImgBytes(availableImage);
 
-    File image = File(path);
-    final image1 = FirebaseVisionImage.fromFile(image);
+    
+    final image1 =  FirebaseVisionImage.fromBytes(availableImage.planes[0].bytes, metadata);
     final faceDetector = FirebaseVision.instance.faceDetector();
     List<Face> faces = await faceDetector.processImage(image1);
-    _faces = new List<Rect>();
+    setState(() {
+    _faces = new List<Rect>();  
+
     for (var i = 0; i < faces.length; i++) {
     _faces.add(faces[i].boundingBox);
     }
-    
+        });
+    log('data: $_faces');
     
     }
 
@@ -153,7 +251,7 @@ import 'dart:async';
         );
       }
 
-      findBound();
+      //findBound();
       return new CustomPaint(
         foregroundPainter: new FacePainter(),
         child: AspectRatio(
@@ -175,7 +273,7 @@ import 'dart:async';
         child: Align(
           alignment: Alignment.centerLeft,
           child: FlatButton.icon(
-              onPressed: _onSwitchCamera,
+              onPressed: null,//_onSwitchCamera,
               icon: Icon(
                   _getCameraLensIcon(lensDirection)
               ),
@@ -191,18 +289,18 @@ import 'dart:async';
     String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
   
     Future<void> _onCameraSwitched(CameraDescription cameraDescription) async {
-      if (controller != null) {
-        await controller.dispose();
+       
+      if(controller != null)
+      {
+        controller.stopImageStream();
+        controller.dispose();
       }
-  
-      controller = CameraController(cameraDescription, ResolutionPreset.high);
-  
+      
+      controller = CameraController(cameraDescription, ResolutionPreset.medium, enableAudio: false);
       // If the controller is updated then update the UI.
+      /*
       controller.addListener(() {
-        if (mounted) {
-          setState(() {});
-        }
-  
+       
         if (controller.value.hasError) {
           Fluttertoast.showToast(
               msg: 'Camera error ${controller.value.errorDescription}',
@@ -214,15 +312,26 @@ import 'dart:async';
           );
         }
       });
-  
+      */
       try {
-        await controller.initialize();
+        controller.initialize().then((_){
+            controller.startImageStream((CameraImage availableImage) {
+         log('data: hello');
+        findBound(availableImage);
+          });
+        });
+         
+
       } on CameraException catch (e) {
         _showCameraException(e);
       }
-  
+      
+      
+
       if (mounted) {
-        setState(() {});
+        setState(() {     
+        });
+
       }
     }
   
@@ -282,6 +391,8 @@ import 'dart:async';
     //canvas.drawPath(path, paint);
     for(int i=0;i<_faces.length;i++)
     canvas.drawRect(_faces[i], paint);
+    
+    //canvas.drawRect( new Rect.fromLTRB(100.0, 50.0, 200.0, 250.0), paint);//_faces[i], paint);
   
   }
 
---
+++ lib/stm.dart
@@ -0,0 +1,310 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:camera/camera.dart';
+import 'package:device_info/device_info.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_widgets/flutter_widgets.dart';
+import 'package:path_provider/path_provider.dart';
+
+part 'utils.dart';
+
+typedef HandleDetection<T> = Future<T> Function(FirebaseVisionImage image);
+typedef Widget ErrorWidgetBuilder(BuildContext context, CameraError error);
+
+enum CameraError {
+  unknown,
+  cantInitializeCamera,
+  androidVersionNotSupported,
+  noCameraAvailable,
+}
+
+enum _CameraState {
+  loading,
+  error,
+  ready,
+}
+
+class CameraMlVision<T> extends StatefulWidget {
+  final HandleDetection<T> detector;
+  final Function(T) onResult;
+  final WidgetBuilder loadingBuilder;
+  final ErrorWidgetBuilder errorBuilder;
+  final WidgetBuilder overlayBuilder;
+  final CameraLensDirection cameraLensDirection;
+  final ResolutionPreset resolution;
+  final Function onDispose;
+
+  CameraMlVision({
+    Key key,
+    @required this.onResult,
+    @required this.detector,
+    this.loadingBuilder,
+    this.errorBuilder,
+    this.overlayBuilder,
+    this.cameraLensDirection = CameraLensDirection.back,
+    this.resolution,
+    this.onDispose,
+  }) : super(key: key);
+
+  @override
+  CameraMlVisionState createState() => CameraMlVisionState<T>();
+}
+
+class CameraMlVisionState<T> extends State<CameraMlVision<T>> {
+  String _lastImage;
+  Key _visibilityKey = UniqueKey();
+  CameraController _cameraController;
+  ImageRotation _rotation;
+  _CameraState _cameraMlVisionState = _CameraState.loading;
+  CameraError _cameraError = CameraError.unknown;
+  bool _alreadyCheckingImage = false;
+  bool _isStreaming = false;
+  bool _isDeactivate = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initialize();
+  }
+
+  Future<void> stop() async {
+    if (_cameraController != null) {
+      if (_lastImage != null && File(_lastImage).existsSync()) {
+        await File(_lastImage).delete();
+      }
+
+      Directory tempDir = await getTemporaryDirectory();
+      _lastImage = '${tempDir.path}/${DateTime.now().millisecondsSinceEpoch}';
+      try {
+        await _cameraController.takePicture(_lastImage);
+      } on PlatformException catch (e) {
+        debugPrint('$e');
+      }
+
+      _stop(false);
+    }
+  }
+
+  void _stop(bool silently) {
+    Future.microtask(() async {
+      if (_cameraController?.value?.isStreamingImages == true && mounted) {
+        await _cameraController.stopImageStream();
+      }
+    });
+
+    if (silently) {
+      _isStreaming = false;
+    } else {
+      setState(() {
+        _isStreaming = false;
+      });
+    }
+  }
+
+  void start() {
+    if (_cameraController != null) {
+      _start();
+    }
+  }
+
+  void _start() {
+    _cameraController.startImageStream(_processImage);
+    setState(() {
+      _isStreaming = true;
+    });
+  }
+
+  CameraValue get cameraValue => _cameraController?.value;
+  ImageRotation get imageRotation => _rotation;
+
+  Future<void> Function() get prepareForVideoRecording =>
+      _cameraController.prepareForVideoRecording;
+
+  Future<void> startVideoRecording(String path) async {
+    await _cameraController.stopImageStream();
+    return _cameraController.startVideoRecording(path);
+  }
+
+  Future<void> stopVideoRecording() async {
+    await _cameraController.stopVideoRecording();
+    await _cameraController.startImageStream(_processImage);
+  }
+
+  Future<void> Function(String path) get takePicture =>
+      _cameraController.takePicture;
+
+  Future<void> _initialize() async {
+    if (Platform.isAndroid) {
+      final deviceInfo = DeviceInfoPlugin();
+      final androidInfo = await deviceInfo.androidInfo;
+      if (androidInfo.version.sdkInt < 21) {
+        debugPrint('Camera plugin doesn\'t support android under version 21');
+        if (mounted) {
+          setState(() {
+            _cameraMlVisionState = _CameraState.error;
+            _cameraError = CameraError.androidVersionNotSupported;
+          });
+        }
+        return;
+      }
+    }
+
+    CameraDescription description =
+        await _getCamera(widget.cameraLensDirection);
+    if (description == null) {
+      _cameraMlVisionState = _CameraState.error;
+      _cameraError = CameraError.noCameraAvailable;
+
+      return;
+    }
+    _cameraController = CameraController(
+      description,
+      widget.resolution ??
+          ResolutionPreset
+              .low, // As the doc says, better to set low when streaming images to avoid drop frames on older devices
+      enableAudio: false,
+    );
+    if (!mounted) {
+      return;
+    }
+
+    try {
+      await _cameraController.initialize();
+    } catch (ex, stack) {
+      debugPrint('Can\'t initialize camera');
+      debugPrint('$ex, $stack');
+      if (mounted) {
+        setState(() {
+          _cameraMlVisionState = _CameraState.error;
+          _cameraError = CameraError.cantInitializeCamera;
+        });
+      }
+      return;
+    }
+
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _cameraMlVisionState = _CameraState.ready;
+    });
+    _rotation = _rotationIntToImageRotation(
+      description.sensorOrientation,
+    );
+
+    //FIXME hacky technique to avoid having black screen on some android devices
+    await Future.delayed(Duration(milliseconds: 200));
+    start();
+  }
+
+  @override
+  void dispose() {
+    if (widget.onDispose != null) {
+      widget.onDispose();
+    }
+    if (_lastImage != null && File(_lastImage).existsSync()) {
+      File(_lastImage).delete();
+    }
+    if (_cameraController != null) {
+      _cameraController.dispose();
+    }
+    _cameraController = null;
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_cameraMlVisionState == _CameraState.loading) {
+      return widget.loadingBuilder == null
+          ? Center(child: CircularProgressIndicator())
+          : widget.loadingBuilder(context);
+    }
+    if (_cameraMlVisionState == _CameraState.error) {
+      return widget.errorBuilder == null
+          ? Center(child: Text('$_cameraMlVisionState $_cameraError'))
+          : widget.errorBuilder(context, _cameraError);
+    }
+
+    Widget cameraPreview = AspectRatio(
+      aspectRatio: _cameraController.value.aspectRatio,
+      child: _isStreaming
+          ? CameraPreview(
+              _cameraController,
+            )
+          : _getPicture(),
+    );
+    if (widget.overlayBuilder != null) {
+      cameraPreview = Stack(
+        fit: StackFit.passthrough,
+        children: [
+          cameraPreview,
+          widget.overlayBuilder(context),
+        ],
+      );
+    }
+    return VisibilityDetector(
+      child: FittedBox(
+        alignment: Alignment.center,
+        fit: BoxFit.cover,
+        child: SizedBox(
+          width: _cameraController.value.previewSize.height *
+              _cameraController.value.aspectRatio,
+          height: _cameraController.value.previewSize.height,
+          child: cameraPreview,
+        ),
+      ),
+      onVisibilityChanged: (VisibilityInfo info) {
+        if (info.visibleFraction == 0) {
+          //invisible stop the streaming
+          _isDeactivate = true;
+          _stop(true);
+        } else if (_isDeactivate) {
+          //visible restart streaming if needed
+          _isDeactivate = false;
+          _start();
+        }
+      },
+      key: _visibilityKey,
+    );
+  }
+
+  void _processImage(CameraImage cameraImage) async {
+    if (!_alreadyCheckingImage && mounted) {
+      _alreadyCheckingImage = true;
+      try {
+        final T results =
+            await _detect<T>(cameraImage, widget.detector, _rotation);
+        widget.onResult(results);
+      } catch (ex, stack) {
+        debugPrint('$ex, $stack');
+      }
+      _alreadyCheckingImage = false;
+    }
+  }
+
+  void toggle() {
+    if (_isStreaming && _cameraController.value.isStreamingImages) {
+      stop();
+    } else {
+      start();
+    }
+  }
+
+  Widget _getPicture() {
+    if (_lastImage != null) {
+      final file = File(_lastImage);
+      if (file.existsSync()) {
+        return Image.file(file);
+      }
+    }
+
+    return Container();
+  }
+}
